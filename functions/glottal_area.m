b9426da413ac343338533e7aa9120cd81bef6a7e
Asterios Toutios <toutios@usc.edu>
2016-02-17 13:32:40 -0800

    Initial commit

---
+++ functions/glottal_area.m
@@ -0,0 +1,33 @@
+function [Agp] = glottal_area(Ap, F0, Fs, D)
+% Ap: peak amplitude
+% F0: fundamental frequency
+% Fs: sampling rate
+% D: duration of sample
+
+Times = []; %vector for plotting Agp vs time
+Agp = []; %returns vector of Agp values
+
+T0 = 1.0 / F0;
+t1 = .36 * T0;
+t2 = .26 * T0;
+a = pi / t1;
+b = 1.0 / (1.0 - cos(pi * t2 / t1));
+dt = 1.0 / Fs;
+
+for n = 1:(Fs * D)
+    Times(n) = n/Fs;
+    t = mod((n * dt), T0);
+    if t < t1
+        Agp(n) = .5 * Ap * (1.0 - cos(a * t));
+    elseif t < (t1 + t2)
+        Agp(n) = Ap * (1.0 - b + b * cos(a * (t - t1)));
+    else
+        Agp(n) = 0;
+    end
+end
+
+figure;
+plot(Times, Agp);
+
+end
+
