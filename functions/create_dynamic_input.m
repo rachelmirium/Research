a4f13ac03d4c9bead5be3441c23f5d69f0d714bd
Asterios Toutios <toutios@usc.edu>
2016-04-06 15:51:33 -0700

    Corrected dynamic input interpolation

--- functions/create_dynamic_input.m
+++ functions/create_dynamic_input.m
@@ -9,15 +9,31 @@ A2 = u.are_A(1,:);
 X1 = i.are_x(1,:);
 X2 = u.are_x(1,:);
 
-X1s=linspace(.3968,X2(end),46);
-A1 = interp1(X1s, A2, X2);
+%X1c = [0 cumsum(X1)];
+X1c = cumsum(X1);
+%X2c = [0 cumsum(X2)];
+X2c = cumsum(X2);
 
-A = zeros(10000, 46);
-x = zeros(10000, 46);
+%X1s=linspace(.3968,X2(end),46);
+X1i = linspace(X1c(1), X1c(end), 100);
+X2i = linspace(X2c(1), X2c(end), 100);
 
-for j = 1:46
-    A(:,j) = transpose(linspace(A1(j), A2(j), 10000));
-    x(:,j) = transpose(linspace(X1s(j), X2(j), 10000));
+%A1 = interp1(X1s, A2, X2);
+% size(A1)
+% size(X1c)
+% size(X1i)
+A1i = interp1(X1c, A1, X1i);
+A2i = interp1(X2c, A2, X2i);
+
+X1id = diff([0 X1i]);
+X2id = diff([0 X2i]);
+
+A = zeros(10000, 100);
+x = zeros(10000, 100);
+
+for j = 1:100
+    A(:,j) = transpose(linspace(A1i(j), A2i(j), 10000));
+    x(:,j) = transpose(linspace(X1id(j), X2id(j), 10000));
 end
 
 end
