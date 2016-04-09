520c5a3898f1ebcd7ba63ab9d4ac7fc20a2f332a
Rachel Alexander <rachelalexander@Rachels-MacBook-Air.local>
2016-04-09 16:59:08 -0700

    file friction_static_vt_shape

---
+++ functions/VCV_dynamic_input.m
@@ -0,0 +1,49 @@
+function [A, x] = VCV_dynamic_input(sound1, sound2, sound3)
+
+v1 = load(sound1);
+c = load(sound2);
+v2 = load(sound3);
+
+M = 100;
+
+A1 = v1.are_A(1,:);
+A2 = c.are_A(1,:);
+A3 = v2.are_A(1,:);
+X1 = v1.are_x(1,:);
+X2 = c.are_x(1,:);
+X3 = v2.are_x(1,:);
+
+X1c = cumsum(X1);
+X2c = cumsum(X2);
+X3c = cumsum(X3);
+
+X1i = linspace(X1c(1), X1c(end), M);
+X2i = linspace(X2c(1), X2c(end), M);
+X3i = linspace(X3c(1), X3c(end), M);
+
+A1i = interp1(X1c, A1, X1i);
+A2i = interp1(X2c, A2, X2i);
+A3i = interp1(X3c, A3, X3i);
+
+X1id = diff([0 X1i]);
+X2id = diff([0 X2i]);
+X3id = diff([0 X3i]);
+
+A = zeros(10000, M);
+x = zeros(10000, M);
+
+for j = 1:M
+    firstA = linspace(A1i(j), A2i(j), 5000);
+    secondA = linspace(A2i(j), A3i(j), 5000);
+    newA = [firstA secondA];
+    
+    firstX = linspace(X1id(j), X2id(j), 5000);
+    secondX = linspace(X2id(j), X3id(j), 5000);
+    newX = [firstX secondX];
+    
+    A(:,j) = transpose(newA(j));
+    x(:,j) = transpose(newX(j));
+end
+
+end
+
--- functions/create_dynamic_input.m
+++ functions/create_dynamic_input.m
@@ -1,37 +1,31 @@
-function [A, x] = create_dynamic_input()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-u = load('../synth_vowels/story_who.mat');
-i = load('../synth_vowels/story_heed.mat');
+function [A, x] = create_dynamic_input(sound1, sound2)
 
-A1 = i.are_A(1,:);
-A2 = u.are_A(1,:);
-X1 = i.are_x(1,:);
-X2 = u.are_x(1,:);
+s1 = load(sound1);
+s2 = load(sound2);
 
-%X1c = [0 cumsum(X1)];
+M = 100;
+
+A1 = s1.are_A(1,:);
+A2 = s2.are_A(1,:);
+X1 = s1.are_x(1,:);
+X2 = s2.are_x(1,:);
+
 X1c = cumsum(X1);
-%X2c = [0 cumsum(X2)];
 X2c = cumsum(X2);
 
-%X1s=linspace(.3968,X2(end),46);
-X1i = linspace(X1c(1), X1c(end), 100);
-X2i = linspace(X2c(1), X2c(end), 100);
+X1i = linspace(X1c(1), X1c(end), M);
+X2i = linspace(X2c(1), X2c(end), M);
 
-%A1 = interp1(X1s, A2, X2);
-% size(A1)
-% size(X1c)
-% size(X1i)
 A1i = interp1(X1c, A1, X1i);
 A2i = interp1(X2c, A2, X2i);
 
 X1id = diff([0 X1i]);
 X2id = diff([0 X2i]);
 
-A = zeros(10000, 100);
-x = zeros(10000, 100);
+A = zeros(10000, M);
+x = zeros(10000, M);
 
-for j = 1:100
+for j = 1:M
     A(:,j) = transpose(linspace(A1i(j), A2i(j), 10000));
     x(:,j) = transpose(linspace(X1id(j), X2id(j), 10000));
 end
