197b212257bd4c9a7181c718b92624ce653d25fd
Rachel Alexander <rachelalexander@Rachels-MacBook-Air.local>
2016-06-28 22:28:20 -0700

    dynamic_glottal_area fixed, synth_with_nt edited but not working- changing values at nasal coupling point causes matrix errors; velocity of nasal tract only outputs zero values

--- functions/dynamic_glottal_area.m
+++ functions/dynamic_glottal_area.m
@@ -4,13 +4,18 @@ N = length(Ap);
 Agp = zeros(N, 1);
 dt = 1.0 / Fs;
 
+T0 = 0.01;%1.0 / F0(100);
+t1 = .36 * T0;
+t2 = .26 * T0;
+a = pi / t1;
+b = 1.0 / (1.0 - cos(a * t2));
+
+n1 = 0;
+
 for n = 1:N
-    T0 = 1.0 / F0(n);
-    t1 = .36 * T0;
-    t2 = .26 * T0;
-    a = pi / t1;
-    b = 1.0 / (1.0 - cos(a * t2));
-    t = mod((n * dt), T0);
+
+    t = (n - n1) * dt; 
+      
     if t < t1
         Agp(n) = .5 * Ap(n) * (1.0 - cos(a * t));
     elseif t < (t1 + t2)
@@ -18,7 +23,18 @@ for n = 1:N
     else
         Agp(n) = .00001;
     end
+    
+    if t > T0
+        
+        n1 = n;
+        if(F0(n) < 1) T0 = .01;
+        else T0 = 1.0 / F0(n);
+        end
+        t1 = .36 * T0;
+        t2 = .26 * T0;
+        a = pi / t1;
+        b = 1.0 / (1.0 - cos(a * t2));
+        
+    end;
+  
 end
-
-end
-
